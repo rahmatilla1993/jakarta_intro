9a4f2928385a333b90316cffb48767c54b2e1248
rahmatilla <rahmatilla301993@gmail.com>
2024-02-17 15:07:17 +0500

    initial commit

---
+++ src/main/java/com/example/intro/HomeServlet.java
@@ -0,0 +1,30 @@
+package com.example.intro;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "HomeServlet",
+        value = "/home"
+)
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("text/html");
+        writer.println("<h1>Welcome to home servlet</h1");
+    }
+}
